fd2ee0a7336dc2df72535c2e734d6359d3599752
Gurnav224 <chaudharyg856@gmail.com>
2024-12-20 14:36:15 +0530

    feat: authentication added

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,73 @@
+const User = require("../models/user.model");
+const generateToken = require("../utils/generateToken");
+
+exports.register = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    // check if user already exists
+
+    const exitingUser = await User.findOne({ email });
+
+    if (exitingUser) {
+      return res.status(400).json({ error: "Email already registered" });
+    }
+
+    // create new user
+
+    const user = new User({
+      name,
+      email,
+      password,
+    });
+
+    await user.save();
+
+    const token = generateToken(user._id);
+
+    res.status(201).json({
+      message: "Registered successfully",
+      token,
+      user: {
+        id: user._id,
+        email: user.email,
+        name: user.name,
+      },
+    });
+  } catch (error) {
+    console.error("failed to register user");
+    res.status(500).json({ error: "failed to register new user" });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(201).json({ error: "Invalid Credentials" });
+    }
+
+    // verify password;
+
+    const isMatch = await user.comparePassword(password);
+
+    if (!isMatch) {
+      return res.status(401).json({ error: "Invalid Credentials" });
+    }
+
+    // generate token
+    const token = generateToken(user._id);
+
+    res.json({
+      message: "Login Successful",
+      token,
+      user: {
+        id: user._id,
+        email: user.email,
+        name: user.name,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ error: "login failed" });
+  }
+};
