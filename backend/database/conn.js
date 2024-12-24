a8fa5171aa315e8abe738a27514d44e7a5eb5930
Gurnav224 <chaudharyg856@gmail.com>
2024-12-24 21:00:50 +0530

    added local db

--- backend/database/conn.js
+++ backend/database/conn.js
@@ -1,18 +1,31 @@
- 
+const mongoose = require("mongoose");
 
- const mongoose = require('mongoose');
+const connetDB = async () => {
+  try {
+    let connnectionUrl;
 
+    console.log(process.env.NODE_ENV)
 
- const connetDB = async () => {
-    try {
-        const connection = await mongoose.connect(process.env.MONGODB,{dbName:'EcommerceDB'});
-        if(connection){
-            console.log(`successfully connected database`)
-        }
-    } catch (error) {
-        console.error('failed to connect database')
+    if (process.env.NODE_ENV === "Production") {
+      connnectionUrl = process.env.MONGODB;
+    } else {
+      connnectionUrl = process.env.localDB;
     }
- }
 
+    const connection = await mongoose.connect(connnectionUrl, {
+      dbName: "EcommerceDB",
+    });
 
- module.exports = connetDB;
+    if (connection) {
+      console.log(
+        `successfully connected database`,
+        connection.connection.host,
+        connection.connection.db.databaseName
+      );
+    }
+  } catch (error) {
+    console.error("failed to connect database");
+  }
+};
+
+module.exports = connetDB;
