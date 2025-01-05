5767a891407461ca3d92edf5ede7172acc9bbd35
Gurnav224 <chaudharyg856@gmail.com>
2025-01-05 16:26:12 +0530

    feat: add user details to footer

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,6 +7,7 @@ import { useAuth } from '../context/AuthContext';
 const Header = ({cart , wishlist}) => {
   const [searchQuery, setSearchQuery] = useState('');
   const { user , logout } = useAuth();
+  const [userDetails, setUserDetails] = useState(false)
 
 
 
@@ -52,11 +53,37 @@ const Header = ({cart , wishlist}) => {
             {user ?  wishlist?.length : 0}
           </span>
         </Link>
-        <Link to='/profile' className="hover:bg-gray-100 p-2 rounded-full">
-          <User className="text-gray-700" size={24} /> 
-          {user && user.name}
+        {/* user profile wrapper */}
+        <div className="relative">
+  <button
+    onClick={() => setUserDetails((prev) => !prev)}
+    className="flex items-center justify-center p-2 rounded-full hover:bg-gray-200 transition duration-200 ease-in-out"
+  >
+    <User  className="text-gray-700" size={24} />
+  </button>
+
+  {userDetails && (
+    <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-lg p-4 z-10">
+      <div className="flex flex-col">
+        <span className="font-semibold text-gray-800">{user && user.name}</span>
+        <Link
+          to="/profile"
+          className="mt-2 text-gray-600 hover:text-blue-500 transition duration-200"
+        >
+          Profile
         </Link>
-          <button onClick={() => logout()}>logout</button>
+        <button
+          onClick={() => logout()}
+          className="mt-2 text-red-600 hover:text-red-500 transition duration-200"
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  )}
+</div>
+        
+         
         <Link to="/cart" className="hover:bg-gray-100 p-2 rounded-full relative">
           <ShoppingCart className="text-gray-700" size={24} />
           <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full px-2 py-1">
