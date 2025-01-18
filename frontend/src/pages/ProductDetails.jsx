32bf8e3a444139f38f3c59e776b5b168f4f257ef
Gurnav224 <chaudharyg856@gmail.com>
2025-01-18 09:46:16 +0530

    active the buynow button functionality

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -80,6 +80,13 @@ const ProductDetails = ({ handleAddToCart, handleAddToWishlist, isProductInCart 
   } 
  
 
+  const handleBuyNow = (product,e) => {
+    handleAddToCart(product , e)
+    setTimeout(() => {
+    navigate('/cart')
+      
+    }, 2000);
+  }
 
 
 
@@ -113,36 +120,10 @@ const ProductDetails = ({ handleAddToCart, handleAddToWishlist, isProductInCart 
           {/* Action Buttons */}
           <div className="flex space-x-4 mt-6">
 
-            <button className="flex-1 flex items-center justify-center bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition duration-300 ease-in-out transform hover:scale-105">
+            <button  onClick={(e) =>handleBuyNow(product,e)} className="flex-1 flex items-center justify-center bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition duration-300 ease-in-out transform hover:scale-105">
              Buy Now
             </button>
-{/* 
-            <button 
-            onClick={(e) => handleAddToCart(product , e)}
-            className="
-            flex-1 
-            flex items-center 
-            justify-center
-             px-4 
-              py-2 
-              border-2 
-              border-blue-500 
-              text-blue-500 
-              rounded-lg 
-              transition 
-              duration-300 
-              ease-in-out 
-              text-center 
-              font-semibold 
-              uppercase 
-              tracking-wider 
-              hover:shadow-md 
-              focus:outline-none 
-              focus:ring-2 
-              focus:ring-blue-500 
-              focus:ring-opacity-50">
-            <ShoppingCart className="mr-2" /> {isProductInCart ? "Go To Cart" : "Add To Cart"}
-            </button> */}
+
 
             {isProductInCart(product._id) ? (
             <button
