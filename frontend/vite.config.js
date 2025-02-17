71366029d8346f87caa0ed6f1aaada53dd258973
Gurnav224 <chaudharyg856@gmail.com>
2025-02-18 05:08:45 +0530

    properly vite.config.js for the productions

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,40 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import process from "node:process";
+import dotenv from "dotenv";
+import path from "path";
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+dotenv.config({ path: path.resolve(__dirname, "./.env") });
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
 
-
   return {
     server: {
+      port: env.VITE_PORT || 3000,
+      open: true,
+
       proxy: {
-        "/api": env?.VITE_API_URL_VERCEL,
+        "/api": {
+          target: env.VITE_API_URL_VERCEL || "http://localhost:3000",
+          changeOrigin: true,
+        },
       },
     },
     plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+
+    build: {
+      outDir: "dist",
+      assetsDir: "assets",
+      sourcemap: true,
+    },
   };
 });
