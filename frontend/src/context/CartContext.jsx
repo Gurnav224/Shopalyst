212aef92903ece14a700b5c9a2a42e6800eabdca
Gurnav224 <chaudharyg856@gmail.com>
2025-02-18 20:42:01 +0530

    refactor cart wishlist context

--- frontend/src/context/CartContext.jsx
+++ frontend/src/context/CartContext.jsx
@@ -23,39 +23,37 @@ export const CartProvider = ({ children }) => {
     }
   }, []);
 
-
- 
-
   const handleAddToCart = async (product, e) => {
     // Stop event propagation to prevent link navigation
     e.stopPropagation();
     e.preventDefault();
     //exiting product cart ;
 
-    toast.success("Item added to cart");
-
     const { _id: productId } = product;
 
     try {
+      toast.success("Item added to cart");
+
       const response = await api.post("/cart", { productId });
 
       console.log("add to cart response", response.data.cart.items);
       setCart(response?.data?.cart?.items);
     } catch (error) {
       console.error("failed to add to cart", error);
-     toast.error('failed to add item to cart')
+      toast.error("failed to add item to cart");
     }
   };
 
   const removeItemFromCart = async (productId) => {
     // console.log('Item to removed from the cart',productId);
-    toast.info("Item removed from cart successfully");
 
     const previousCartList = [...cart];
 
     setCart((prev) => prev.filter((item) => item.product._id !== productId));
 
     try {
+      toast.info("Item removed from cart successfully");
+
       const response = await api.delete(`/cart`, { data: { productId } });
       if (
         response?.data?.message !== "item from remove from cart successfully"
@@ -64,13 +62,11 @@ export const CartProvider = ({ children }) => {
         setCart(previousCartList);
       }
     } catch (error) {
-      console.error("failed to remove product", error?.response?.data?.error);
+      toast.error(error?.response?.data?.error || "failed to remove product");
 
       setCart(previousCartList);
     }
   };
-
-  
 
   const isProductInCart = (productId) => {
     return cart?.some((item) => item?.product?._id === productId);
@@ -91,29 +87,19 @@ export const CartProvider = ({ children }) => {
       : [...cart, { ...product, quantity: 1 }];
 
     try {
-
-        console.log(updatedCartList)
-
       setCart(updatedCartList);
 
-      const response = await api.put("/cart/update", { productId, action });
-
-      console.log('quantity updated response ',response?.data?.cart);
-
-
-
+      await api.put("/cart/update", { productId, action });
     } catch (error) {
-      console.error("failed to update item quantity", error?.response?.data?.error);
+      toast.error(
+        error?.response?.data?.error || "failed to update item quantity"
+      );
     }
   };
 
-
-
-useEffect(() => {
-    fetchCart()
-},[fetchCart]);
- 
-
+  useEffect(() => {
+    fetchCart();
+  }, [fetchCart]);
 
   return (
     <CartContext.Provider
