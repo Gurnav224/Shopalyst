fcc8569223efdda44c30407ca85398764bb649b1
Gurnav224 <chaudharyg856@gmail.com>
2025-02-18 20:23:06 +0530

    changes /api fetch ==> /api axios

--- frontend/src/api/cart.js
+++
@@ -1,8 +0,0 @@
-import {  apiRequest } from "./index";
-
-export const cartAPI = {
-    addToCart: (productId, quantity) => apiRequest("cart","POST", {productId, quantity}),
-    fetchCart:() => apiRequest('cart'),
-    removeItemFromCart:(productId) => apiRequest(`cart`,"DELETE", {productId}),
-    updateCartItemQuanity:(productId, action) => apiRequest("cart/update", "PUT", {productId, action}) 
-}
